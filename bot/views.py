fd2dd53a33facb3b348388b600165477c50a7819
Anna Veselova <anna.veselova.97@mail.ru>
2023-09-30 00:46:17 +0300

    add check_subscriptions

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,5 @@
+import calendar
+from datetime import timedelta, date
 from django.shortcuts import render, get_object_or_404
 from bot.models import Client
 
@@ -7,5 +9,8 @@ def is_sub_paid(id_telegram):
 	return user.is_paid_up
 
 
+def get_new_payment_date(curr_date: date = date.today()) -> date:
+	days_in_month = calendar.monthrange(curr_date.year, curr_date.month)[1]
+	return curr_date + timedelta(days=days_in_month)
 
 
